497d62c3fb5de860b52ef8f6016ff3da3f5d5df2
Ethan Yim <liddery69@gmail.com>
2022-01-05 12:09:43 -0800

    hotfix 2

--- components/MiniDahlia.js
+++ components/MiniDahlia.js
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default () => {
   return (
-    <img width='20' height='20' src={'/a/images/mini-dahlia.svg'} />
+    <img alt='mini dahlia logo' width='20' height='20' src={'/a/images/mini-dahlia.svg'} />
   )
 }
