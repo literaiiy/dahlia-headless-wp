47d69bc1482b1cc4f6a160af4fe0b45e329fb91e
Ethan Yim <liddery69@gmail.com>
2022-01-06 12:37:08 -0800

    Google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,17 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-9XPMCZ55HW"></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-9XPMCZ55HW');
+          `}}>
+          </script>
         </Head>
         <body>
           <div className="page">
