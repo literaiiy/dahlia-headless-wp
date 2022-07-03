abf8b512003147facdbb2cb777a8d137bd213290
Ethan Yim <liddery69@gmail.com>
2022-07-03 00:16:37 -0700

    Added sitemap & `Link` for logo

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+
 export default function Header() {
   return (
     <header>
-      <a className="header_image" href="/"><img height="135" width="346" src="/a/images/dahlia-nextjs-smaller.webp" alt="dah.li/a" /></a>
+      <Link className="header_image" href="/">
+        <a>
+          <img height="135" width="346" src="/a/images/dahlia-nextjs-smaller.webp" alt="dah.li/a" />
+        </a>
+      </Link>
       <p className="header_tagline">literaiiy's personal blog</p>
       <hr />
     </header>
---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://dah.li/a',
+  generateRobotsTxt: true, 
+}
+
+// export default config
